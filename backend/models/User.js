e07b08875a9d1d0f184fcf2d6e8b04a18fcdfcbe
rakotomalalasafidy81-lgtm <rakotomalalasafidy81@gmail.com>
2026-09-12 18:34:47 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -36,11 +36,12 @@ const userSchema = new mongoose.Schema({
 // Hash password avant de sauvegarder
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 // Méthode pour comparer les mots de passe
